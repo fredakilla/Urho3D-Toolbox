28d1e403150cd32c68f932909dea93dd6f35aff6
fredakilla <fredakilla@gmail.com>
2018-12-04 06:46:30 +0100

    add ToolBoxObject class

---
+++ Toolbox/Common/ToolBoxObject.cpp
@@ -0,0 +1,95 @@
+#include "ToolBoxObject.h"
+#include <Urho3D/Urho3DAll.h>
+#include "SystemUI/SystemUI.h"
+
+namespace Urho3D
+{
+
+ToolBoxObject::ToolBoxObject(Context* context)
+{
+    context_ = context;
+}
+
+Engine* ToolBoxObject::GetEngine() const
+{
+    return context_->GetSubsystem<Engine>();
+}
+
+Time* ToolBoxObject::GetTime() const
+{
+    return context_->GetSubsystem<Time>();
+}
+
+WorkQueue* ToolBoxObject::GetWorkQueue() const
+{
+    return context_->GetSubsystem<WorkQueue>();
+}
+
+FileSystem* ToolBoxObject::GetFileSystem() const
+{
+    return context_->GetSubsystem<FileSystem>();
+}
+
+#if URHO3D_LOGGING
+Log* ToolBoxObject::GetLog() const
+{
+    return context_->GetSubsystem<Log>();
+}
+#endif
+
+ResourceCache* ToolBoxObject::GetCache() const
+{
+    return context_->GetSubsystem<ResourceCache>();
+}
+
+Localization* ToolBoxObject::GetLocalization() const
+{
+    return context_->GetSubsystem<Localization>();
+}
+
+#if URHO3D_NETWORK
+Network* ToolBoxObject::GetNetwork() const
+{
+    return context_->GetSubsystem<Network>();
+}
+#endif
+
+Input* ToolBoxObject::GetInput() const
+{
+    return context_->GetSubsystem<Input>();
+}
+
+Audio* ToolBoxObject::GetAudio() const
+{
+    return context_->GetSubsystem<Audio>();
+}
+
+UI* ToolBoxObject::GetUI() const
+{
+    return context_->GetSubsystem<UI>();
+}
+
+SystemUI* ToolBoxObject::GetSystemUI() const
+{
+    return context_->GetSubsystem<SystemUI>();
+}
+
+Graphics* ToolBoxObject::GetGraphics() const
+{
+    return context_->GetSubsystem<Graphics>();
+}
+
+Renderer* ToolBoxObject::GetRenderer() const
+{
+    return context_->GetSubsystem<Renderer>();
+}
+
+#if URHO3D_TASKS
+Tasks* ToolBoxObject::GetTasks() const
+{
+    return context_->GetSubsystem<Log>();
+}
+#endif
+
+
+}
---
+++ Toolbox/Common/ToolBoxObject.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <Urho3D/Core/Context.h>
+
+namespace Urho3D
+{
+
+class Context;
+class EventHandler;
+class Engine;
+class Time;
+class WorkQueue;
+#if URHO3D_PROFILING
+class Profiler;
+#endif
+class FileSystem;
+#if URHO3D_LOGGING
+class Log;
+#endif
+class ResourceCache;
+class Localization;
+#if URHO3D_NETWORK
+class Network;
+#endif
+class Input;
+class Audio;
+class UI;
+class SystemUI;
+class Graphics;
+class Renderer;
+#if URHO3D_TASKS
+class Tasks;
+#endif
+
+
+class ToolBoxObject
+{
+public:
+
+    ToolBoxObject(Context* context);
+
+    /// Return engine subsystem.
+    Engine* GetEngine() const;
+    /// Return time subsystem.
+    Time* GetTime() const;
+    /// Return work queue subsystem.
+    WorkQueue* GetWorkQueue() const;
+    /// Return file system subsystem.
+    FileSystem* GetFileSystem() const;
+#if URHO3D_LOGGING
+    /// Return logging subsystem.
+    Log* GetLog() const;
+#endif
+    /// Return resource cache subsystem.
+    ResourceCache* GetCache() const;
+    /// Return localization subsystem.
+    Localization* GetLocalization() const;
+#if URHO3D_NETWORK
+    /// Return network subsystem.
+    Network* GetNetwork() const;
+#endif
+    /// Return input subsystem.
+    Input* GetInput() const;
+    /// Return audio subsystem.
+    Audio* GetAudio() const;
+    /// Return UI subsystem.
+    UI* GetUI() const;
+    /// Return system ui subsystem.
+    SystemUI* GetSystemUI() const;
+    /// Return graphics subsystem.
+    Graphics* GetGraphics() const;
+    /// Return renderer subsystem.
+    Renderer* GetRenderer() const;
+#if URHO3D_TASKS
+    /// Return tasks subsystem.
+    Tasks* GetTasks() const;
+#endif
+
+private:
+    Context* context_;
+
+};
+
+}
